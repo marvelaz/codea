c4255b0a47c22921b1766b85f239789e11a1032a
Mario Velazquez <marvelaz@marvelaz.local>
2017-07-05 12:02:08 -0500

    second commit

---
+++ array.rb
@@ -0,0 +1,32 @@
+array = [1,2,3,4,5]
+i=0
+b=[]
+
+array.each do |x|
+  b[i] = x + 20
+  i += 1
+end
+
+array = ["c", "b", "a"]
+factor = 2
+i = 0
+result=[ ]
+a=*(0..factor)
+
+array.each do |x|
+        a.each do |y|
+            result[i] = [x,y]
+        end
+        i += 1
+end
+a = 0
+b =* (1..10)
+c =* (1..5)
+
+c.each do |x|
+  b.each do |y|
+    a = y * x
+    print "#{a}" + "  "
+  end
+  print "\n"
+end
